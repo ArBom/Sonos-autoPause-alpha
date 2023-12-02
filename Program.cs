7fc15be3adf023fc5f866e44ba93837189f6c3be
Arek Bomba <59375967+ArBom@users.noreply.github.com>
2023-12-02 22:10:03 +0100

    Fix very stupid issue.

--- Program.cs
+++ Program.cs
@@ -2,22 +2,49 @@
 using ByteDev.Sonos.Device;
 using ByteDev.Sonos.Models;
 using Sonos_autoPause_alpha;
+using System.Net;
 
 public class Program
 {
     public static async Task Main(string[] args)
     {
-        SonosController controller = new SonosControllerFactory().Create("192.168.1.100");
+        bool ProvideForFocusSession;
+        List<string> SonosesIPs = new List<string>();
 
-        List<SonosController> ConnectedSonosControllers = new List<SonosController>();
-        List<SonosController> PausedHereSonosControllers = new List<SonosController>();
+        if (args.Count() == 0)
+        {
+            ProvideForFocusSession = true;
+            SonosesIPs.Add("169.254.212.11");
+
+        }
+        else
+        {
+            if (!Boolean.TryParse(args[0], out ProvideForFocusSession))
+            {
+                Console.WriteLine("Wrong arg. of provideing for focus session.");
+                ProvideForFocusSession = true;
+            }
+
+            for (int i = 1; i!= args.Length; ++i)
+            {
+                if (IPAddress.TryParse(args[i], out _))
+                    SonosesIPs.Add(args[i]);
+                else
+                    Console.WriteLine("Wrong arg. of sonos ip - arg no " + i);
+            }
+        }
+
+        SonosController controller = new SonosControllerFactory().Create("169.254.212.11");
+
+        ConnectedSonosControllers = new List<SonosController>();
+        PausedHereSonosControllers = new List<SonosController>();
 
         ConnectedSonosControllers.Add(controller);
 
         SystemMediaObserver systemMediaObserver = new(true);
         systemMediaObserver.fadeMusicOut += PlayPauseControllers;
 
-        Console.WriteLine("Press any key to quit..");
+        Console.WriteLine("Programm is running, press any key to quit..");
         Console.ReadKey(true);
     }
 
